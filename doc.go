1673192770226a09bc818d2637797dec5487a554
Roman Sharkov <roman.scharkov@gmail.com>
2022-10-24 17:13:12 +0200

    docs: Add doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package gqt provides a GraphQL Query Template language (GQT) parser.
+// The parser can be used in both schemaless and schema-aware modes.
+// To use the schema-aware mode create a parser instance using NewParser
+// and provide all .graphqls schema files.
+// gqt.Parse will parse the template in schemaless mode.
+package gqt
